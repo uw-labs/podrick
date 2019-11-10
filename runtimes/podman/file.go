1b3ed2cba3fb6f1c22e45ee2965b9d6414f91fba
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-11-10 15:38:35 +0000

    runtimes/podman: support file uploads

---
+++ runtimes/podman/file.go
@@ -0,0 +1,59 @@
+package podman
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/uw-labs/podrick"
+	podman "github.com/uw-labs/podrick/runtimes/podman/iopodman"
+	"github.com/varlink/go/varlink"
+)
+
+func uploadFiles(ctx context.Context, conn *varlink.Connection, cID string, files ...podrick.File) (err error) {
+	mountDir, err := podman.MountContainer().Call(ctx, conn, cID)
+	if err != nil {
+		return fmt.Errorf("failed to mount container filesystem: %w", err)
+	}
+	defer func() {
+		uErr := podman.UnmountContainer().Call(context.Background(), conn, cID, true)
+		if err == nil {
+			err = uErr
+		}
+	}()
+
+	for _, f := range files {
+		err = uploadFile(ctx, mountDir, f)
+		if err != nil {
+			return fmt.Errorf("failed to upload file: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func uploadFile(ctx context.Context, mountDir string, file podrick.File) (err error) {
+	path := filepath.Clean(file.Path)
+	if !filepath.IsAbs(path) {
+		return fmt.Errorf("file paths must be absolute: %q", file.Path)
+	}
+	dest := filepath.Join(mountDir, path)
+	target, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer func() {
+		cErr := target.Close()
+		if err == nil {
+			err = cErr
+		}
+	}()
+	_, err = io.Copy(target, file.Content)
+	if err != nil {
+		return fmt.Errorf("failed to copy file contents: %w", err)
+	}
+
+	return nil
+}
